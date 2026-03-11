be59d2f926bac08f2fbe4f3b93d54fb504a9037a
zerok13 <zerok13@gmail.com>
2026-03-11 18:24:11 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
-import Header from "@/components/Header";
 import { HeroSection } from "@/components/HeroSection";
 import { EventBanner } from "@/components/EventBanner";
 import { NoticeSection } from "@/components/NoticeSection";
 import { AboutSection } from "@/components/AboutSection";
 import { QuickLinks } from "@/components/QuickLinks";
-import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <HeroSection />
-        <EventBanner />
-        <NoticeSection />
-        <AboutSection />
-        <QuickLinks />
-      </main>
-      <Footer />
-    </div>
+    <main className="flex-1">
+      <HeroSection />
+      <EventBanner />
+      <NoticeSection />
+      <AboutSection />
+      <QuickLinks />
+    </main>
   );
 }
